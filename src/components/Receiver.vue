8216791985243f69eb19ca691e8121805bb2a079
Dafeigy <1070642765@qq.com>
2025-05-16 17:00:45 +0800

    <Update>: Receiver done.

--- src/components/Receiver.vue
+++ src/components/Receiver.vue
@@ -25,23 +25,30 @@ const url =
 <template>
     <div id="con" class="flex h-[100vh] flex-col items-center bg-[#202020] p-2">
         <div id="TODO" class="w-[50vmin] h-[10%]  items-center  flex text-theme text-left text-4xl font-display">PROJECT OPHICULUS</div>
-        <div id="status" class=" w-[50vmin] inline-block" >
-             <el-collapse v-model="activeNames" @change="handleChange">
-                <el-collapse-item title="Blocks" name="1">
-                    <el-space wrap><div v-for="item in 64" class="rounded bg-orange w-[0.8vmax] h-[0.8vmax] items-center flex justify-center">
-                    {{ item }}
-                    </div></el-space>
-                    
-                </el-collapse-item>
-            </el-collapse>
+        <div id="status" class=" w-[50vmin] flex justify-center" >
+            <el-card style="width: 95%">
+                <template #header>
+                    <div class="card-header font-display ">
+                    <span>RECEIVING BLOCKS</span>
+                </div>
+                </template>
+                <el-space wrap>
+                    <div v-for="item in 64" class="rounded bg-orange w-[0.8vmax] h-[0.8vmax] items-center flex justify-center font-[100px]">
+                        {{ item }}
+                    </div>
+                </el-space>
+            </el-card>
         </div>
-        <div id="progress" class="w-[50vmin]">
-            <el-progress :percentage="50" class="text-white"/>
+        <div id="progress" class="w-[50vmin] mt-[2%] flex items-center justify-center">
+                <el-progress :percentage="98" class="text-white w-[95%]" color="#b2653b"/>
         </div>
         <div id="img" class=" w-[50vmin] h-[50vmin]  items-center justify-center flex">
-            <el-empty>
-            </el-empty>
-
+            <el-card style="width: 95%">
+                <img
+                    src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+                    style="width: 100%"
+                    />
+            </el-card>
         </div>
         <div id="control" class=" w-[50vmin] h-[5%]  items-center justify-center flex rounded">
             <el-button type="success" color="#5c7f71">Turn on Camera</el-button>
