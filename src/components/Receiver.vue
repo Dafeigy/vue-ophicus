29f26b8eb3c321ec4b0bbe7e06f1cb3fafd0776f
Dafeigy <1070642765@qq.com>
2025-05-17 21:13:39 +0800

    <Update>: Style unify

--- src/components/Receiver.vue
+++ src/components/Receiver.vue
@@ -1,21 +1,17 @@
 <script setup lang="ts">
-// encode("123")
-// ES6 import
 import jsQR from "jsqr";
 import { encode, decode } from 'js-base64';
+import {
+  renderSVG,
+} from 'uqr'
 
 
-import { ImageProps } from 'element-plus'
-
-const fits = [
-  'fill',
-  'contain',
-  'cover',
-  'none',
-  'scale-down',
-] as ImageProps['fit'][]
-const url =
-  'https://fuss10.elemecdn.com/e/5d/4a731a905s94a4af544c0c25941171jpeg.jpeg'
+const svgg = renderSVG("PROJECT OPHICULUS PROJECT OPHICULUSPROJECT OPHICULUSPROJECT OPHICULUSPROJECT OPHICULUSPROJECT OPHICULUSPROJECT OPHICULUSPROJECT OPHICULUSPROJECT OPHICULUSPROJECT OPHICULUSPROJECT OPHICULUSPROJECT OPHICULUSPROJECT OPHICULUSPROJECT OPHICULUSPROJECT OPHICULUSPROJECT OPHICULUSPROJECT OPHICULUSPROJECT OPHICULUSPROJECT OPHICULUSPROJECT OPHICULUSPROJECT OPHICULUSPROJECT OPHICULUSPROJECT OPHICULUSPROJECT OPHICULUSPROJECT OPHICULUSPROJECT OPHICULUSPROJECT OPHICULUSPROJECT OPHICULUSPROJECT OPHICULUSPROJECT OPHICULUSPROJECT OPHICULUSPROJECT OPHICULUSPROJECT OPHICULUSPROJECT OPHICULUSPROJECT OPHICULUSPROJECT OPHICULUSPROJECT OPHICULUSPROJECT OPHICULUSPROJECT OPHICULUSPROJECT OPHICULUSPROJECT OPHICULUSPROJECT OPHICULUSPROJECT OPHICULUSPROJECT OPHICULUSPROJECT OPHICULUSPROJECT OPHICULUSPROJECT OPHICULUSPROJECT OPHICULUSPROJECT OPHICULUSPROJECT OPHICULUSPROJECT OPHICULUS", {
+    pixelSize : 12,
+    whiteColor : '#1D1E1F',
+    blackColor :'#f5eddc',
+    }
+)
 
 </script>
 
@@ -27,31 +23,27 @@ const url =
         <div id="status" class=" w-[50vmin] flex justify-center flex-col" >
 
             <div class="card-header font-display text-3xl bg-orange px-4 mt-6">
-                RECEIVING BLOCKS
+                ▧ RECEIVING STATUS▸
             </div>
-            <div id="blocks-display" class="grid grid-cols-16 gap-1 mt-2 px-4">
-                    <div v-for="item in 63" class="rounded text-sm bg-theme text-green w-[0.8vmax] h-[0.8vmax] flex justify-center items-center">
+            <div id="blocks-display" class="grid grid-cols-16 gap-1 mt-2 px-4 grid-rows-6">
+                    <div v-for="item in 95" class="rounded text-sm  border-1 text-theme w-[0.8vmax] h-[0.8vmax] flex justify-center items-center">
                         {{ item }}
                     </div>
                     <div>...</div>
             </div>
                 
-           
         </div>
-        <div id="progress" class="w-[50vmin] mt-[2%] flex items-center justify-center">
+        <div id="progress" class="w-[50vmin] mt-4 flex items-center justify-center">
                 <el-progress :percentage="43" class="text-white w-[95%]" color="pink"/>
         </div>
         <div id="img" class=" w-[50vmin] h-[50vmin]  items-center justify-center flex">
             <el-card style="width: 95%">
-                <img
-                    src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
-                    style="width: 100%"
-                    />
+                 <div v-html="svgg" class="qrcode"></div>
             </el-card>
         </div>
-        <div id="control" class=" w-[50vmin] h-[5%]  items-center justify-center flex rounded">
-            <el-button type="success" color="#5c7f71">Turn on Camera</el-button>
-            <el-button type="warning" color="#b2653b">Back to Trans</el-button>
+        <div id="control" class=" w-[50vmin] h-[5%] grid-cols-2 grid px-4 text-theme">
+            <el-button type="success" color="#5c7f71">TURN ON CAMERA</el-button>
+            <el-button type="warning" color="#ba8530">DOWNLOAD</el-button>
         </div>
     </div>
 
--- src/components/Sender.vue
+++ src/components/Sender.vue
@@ -3,11 +3,7 @@
 // ES6 import
 import jsQR from "jsqr";
 import {
-  encode,
-  renderANSI,
   renderSVG,
-  renderUnicode,
-  renderUnicodeCompact,
 } from 'uqr'
 
 
@@ -18,25 +14,6 @@ const svgg = renderSVG("PROJECT OPHICULUS PROJECT OPHICULUSPROJECT OPHICULUSPROJ
     }
 )
 
-const ansi = renderUnicodeCompact('PROJECT OPHICULUS', {
-  ecc: 'L',
-  // Border width
-  border: 1,
-})
-
-// display QR Code in terminal
-console.log(ansi)
-import { ImageProps } from 'element-plus'
-
-const fits = [
-  'fill',
-  'contain',
-  'cover',
-  'none',
-  'scale-down',
-] as ImageProps['fit'][]
-const url =
-  'https://fuss10.elemecdn.com/e/5d/4a731a905s94a4af544c0c25941171jpeg.jpeg'
 
 </script>
 
@@ -47,15 +24,15 @@ const url =
         </div>
         <div id="status" class=" w-[50vmin] flex justify-center font-display flex-col " >
             <div class="card-header font-display text-3xl bg-orange px-4 mt-6">
-                ENCODE STATUS
+                ▧ ENCODE STATUS▸
             </div>
-            <div id="details" class="grid grid-cols-3 mt-2 px-4 gap-2 ">
-                <p class="bg-green text-theme px-1">FILENAME:</p> <p class="col-span-2"> Helloworld.md</p>
-                <p class="bg-green text-theme px-1">BLOCKS INDICES</p><p class="col-span-2">[1,2,3,4,5]</p>
-                <p class="bg-green text-theme px-1">TOTAL BLOCKS</p><p class="col-span-2">68</p>
-                <p class="bg-green text-theme px-1">BYTES</p><p class="col-span-2">529.21KB</p>
-                <p class="bg-green text-theme px-1">BITRATE</p ><p class="col-span-2">20</p>
-                <p class="bg-green text-theme px-1" >FPS</p><p class="col-span-2">23.1</p>
+            <div id="details" class="grid grid-cols-5 mt-2 px-4 gap-2 ">
+                <p class="bg-green text-theme px-1 col-span-2">▣ FILENAME:</p> <p class="col-span-3 "> Helloworld.md</p>
+                <p class="bg-green text-theme px-1 col-span-2">▣ BLOCKS INDICES</p><p class="col-span-3 ">[1,2,3,4,5]</p>
+                <p class="bg-green text-theme px-1 col-span-2">▣ TOTAL BLOCKS</p><p class="col-span-3 ">68</p>
+                <p class="bg-green text-theme px-1 col-span-2">▣ BYTES</p><p class="col-span-3 ">529.21KB</p>
+                <p class="bg-green text-theme px-1 col-span-2">▣ BITRATE</p ><p class="col-span-3 ">20</p>
+                <p class="bg-green text-theme px-1 col-span-2">▣ FPS</p><p class="col-span-3 ">23.1</p>
             </div>
             
         </div>
@@ -64,9 +41,9 @@ const url =
                 <div v-html="svgg" class="qrcode"></div>
             </el-card>
         </div>
-        <div id="control" class=" w-[50vmin] h-[5%]  items-center justify-center flex mt-6">
+        <div id="control" class=" w-[50vmin] h-[5%] grid-cols-2 grid px-4 text-theme">
             <el-button type="success" color="#5c7f71">START STRAMING</el-button>
-            <el-button type="warning" color="#802520">SELECT FILE </el-button>
+            <el-button type="warning" color="#ba8530">SELECT FILE </el-button>
         </div>
     </div>
 
